1815ff9bc771e3628f84478bd1b8fdb3d462a9de
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-19 19:07:04 +0600

    dashboard done

--- app/src/main/java/com/example/policecommunication/MainActivity.java
+++ app/src/main/java/com/example/policecommunication/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.policecommunication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    CardView attendanceBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // id Define
+        attendanceBtn = findViewById(R.id.attendenceBtn);
+
+
+
+        // attendance view
+        attendanceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
