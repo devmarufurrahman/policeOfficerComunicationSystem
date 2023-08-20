86bfe8b1e53403895d7ec2d692b68ec4865db9aa
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-20 15:48:29 +0600

    map intrigation successfully

--- app/src/main/java/com/example/policecommunication/LiveLocation.java
+++ app/src/main/java/com/example/policecommunication/LiveLocation.java
@@ -1,14 +1,102 @@
 package com.example.policecommunication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class LiveLocation extends AppCompatActivity {
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.policecommunication.databinding.ActivityLiveLocationBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class LiveLocation extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityLiveLocationBinding binding;
+    Button getLiveLocation;
+    private final int PERMISSION_ID = 44;
+    Location currentLocation ;
+
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_live_location);
+
+        getLiveLocation = findViewById(R.id.getLiveLocation);
+
+        binding = ActivityLiveLocationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(LiveLocation.this);
+
+
+        // location
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(LiveLocation.this);
+
+//        getCurrentLocation();
+//        getLiveLocation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                getCurrentLocation();
+//            }
+//        });
+
     }
+
+//    public void getCurrentLocation(){
+//        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+//            @Override
+//            public void onSuccess(Location location) {
+//           if (location != null){
+//               currentLocation = location;
+//
+//           }
+//            }
+//        });
+//
+//    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng dhaka = new LatLng(23.777176, 90.399452);
+        mMap.addMarker(new MarkerOptions().position(dhaka).title("Marker in Dhaka"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(dhaka));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(dhaka,18f));
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        if (requestCode == PERMISSION_ID){
+//            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+//                getCurrentLocation();
+//            } else {
+//                Toast.makeText(this, "Location permission denied", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 }
