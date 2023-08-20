00d3ff1d37e0dd7aea54e9c16f0d266edf7bda6f
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-20 09:46:10 +0600

    custom list view layout done

--- app/src/main/java/com/example/policecommunication/ViewUserActivity.java
+++ app/src/main/java/com/example/policecommunication/ViewUserActivity.java
@@ -3,12 +3,16 @@ package com.example.policecommunication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ViewUserActivity extends AppCompatActivity {
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_list_user);
+        setContentView(R.layout.view_user_activity);
+        // Id define
+        listView = findViewById(R.id.viewActivityList);
     }
 }
