655b4118105de096646a37d2be84f05a11b6dcba
devmarufurrahaman <mdmarufur5682@gmail.com>
2023-08-21 12:18:39 +0600

    chat layout done

--- app/src/main/java/com/example/policecommunication/MessagePage.java
+++ app/src/main/java/com/example/policecommunication/MessagePage.java
@@ -3,8 +3,12 @@ package com.example.policecommunication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MessagePage extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
